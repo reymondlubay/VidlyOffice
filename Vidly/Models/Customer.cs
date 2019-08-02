ad31e87b0fc718e2b300eeb64103eb4098904b0f
Reymond <reymond.lubay@gmail.com>
2019-08-02 15:52:13 +0800

    Change Data annotation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength (300)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
